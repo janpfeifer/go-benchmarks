db01671fc8c264253907fd48b15f4ea184af3f7a
Jan Pfeifer <pfeifer@gmail.com>
2024-12-15 11:34:49 +0100

    PrettyPrint time duration and tests.

--- prettyprint.go
+++ prettyprint.go
@@ -1,1 +1,45 @@
 package benchmarks
+
+import (
+	"fmt"
+	"time"
+)
+
+func PrettyPrint(d time.Duration) string {
+	if d < time.Microsecond {
+		return fmt.Sprintf("%dns", int(d/time.Nanosecond))
+	}
+	if d < time.Millisecond {
+		return fmt.Sprintf("%.1fµs", float64(d)/float64(time.Microsecond))
+	}
+	if d < time.Second {
+		return fmt.Sprintf("%.1fms", float64(d)/float64(time.Millisecond))
+	}
+	if d < 5*time.Minute {
+		return fmt.Sprintf("%.1fs", float64(d)/float64(time.Second))
+	}
+	if d < time.Hour {
+		minutes := d / time.Minute
+		seconds := float64(d-(minutes*time.Minute)) / float64(time.Second)
+		return fmt.Sprintf("%dm%04.1fs", minutes, seconds)
+	}
+	if d < 24*time.Hour {
+		hours := d / time.Hour
+		remainder := d - (hours * time.Hour)
+		minutes := remainder / time.Minute
+		remainder -= minutes * time.Minute
+		seconds := remainder / time.Second
+		return fmt.Sprintf("%dh%02dm%02ds", hours, minutes, seconds)
+	}
+
+	// Multi-day duration:
+	Day := 24 * time.Hour
+	days := d / Day
+	remainder := d - (days * Day)
+	hours := remainder / time.Hour
+	remainder -= hours * time.Hour
+	minutes := remainder / time.Minute
+	remainder -= minutes * time.Minute
+	seconds := remainder / time.Second
+	return fmt.Sprintf("%dd %dh%02dm%02ds", days, hours, minutes, seconds)
+}
--- prettyprint_test.go
+++ prettyprint_test.go
@@ -1,1 +1,17 @@
 package benchmarks
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestPrettyPrint(t *testing.T) {
+	require.Equal(t, "123ns", PrettyPrint(123*time.Nanosecond))
+	require.Equal(t, "1.3µs", PrettyPrint(1270*time.Nanosecond))
+	require.Equal(t, "1.2ms", PrettyPrint(1230*time.Microsecond))
+	require.Equal(t, "180.3s", PrettyPrint(180280*time.Millisecond))
+	require.Equal(t, "5m07.2s", PrettyPrint(5*time.Minute+7200*time.Millisecond))
+	require.Equal(t, "3h05m07s", PrettyPrint(3*time.Hour+5*time.Minute+7200*time.Millisecond))
+	require.Equal(t, "100d 3h05m07s", PrettyPrint(100*24*time.Hour+3*time.Hour+5*time.Minute+7200*time.Millisecond))
+}
